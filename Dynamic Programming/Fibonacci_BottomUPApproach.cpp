3a7e685918d3c94168f4e79d042b8dc6f2936332
Ayush Choudhary <ayushhurdey999@gmail.com>
2020-07-06 15:39:23 +0530

    Some modifications added...

--- Dynamic Programming/Fibonacci_BottomUPApproach.cpp
+++ Dynamic Programming/Fibonacci_BottomUPApproach.cpp
@@ -25,3 +25,34 @@ int main()
     store=NULL;
     return 0;
 }
+
+
+//-----------------------Little Modifications-----------------//
+
+#include <iostream>
+using namespace std;
+
+int fiboBU(int *dp,int n){
+    dp[0]=0;
+    dp[1]=1;
+    cout << dp[0] <<" " << dp[1] << " ";
+    for(int i=2;i<n+1;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+        cout << dp[i] << " ";
+    }
+    cout << "\n";
+    return dp[n];
+}
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    int *store = new int[n+1];    // dynamic array allocation in c++.
+    cout << fiboBU(store,n);
+    delete [] store;
+    store=NULL;
+    return 0;
+}
+
