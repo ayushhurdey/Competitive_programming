96c090ed79aa2c1792189c618241b7a6fb870c5c
Ayush Choudhary <ayushhurdey999@gmail.com>
2020-08-20 18:21:22 +0530

    Create isSymmetricTree.java

---
+++ tree/binary Tree/isSymmetricTree.java
@@ -0,0 +1,55 @@
+
+/*
+Symmetric Tree 
+Given a Binary Tree. Check whether it is Symmetric or not, i.e. whether the binary tree is a Mirror image of itself or not.
+
+Example 1:
+
+Input:
+         5
+       /   \
+      1     1
+     /       \
+    2         2
+Output: True
+Explanation: Tree is mirror image of
+itself i.e. tree is symmetric
+Example 2:
+
+Input:
+         5
+       /   \
+      10     10
+     /  \     \
+    20  20     30
+Output: False
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function isMirror() which takes the root of the Binary Tree as its input and returns True if the given Binary Tree is a same as the Mirror image of itself. Else, it returns False.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Height of the Tree).
+
+Constraints:
+1<=Number of nodes<=100
+*/
+
+class GfG
+{
+    static boolean isMirror(Node root1,Node root2){
+        if(root1 == null && root2 == null)
+            return true;
+        if(root1 == null || root2 == null)
+            return false;
+        return (root1.data == root2.data) &&
+               isMirror(root1.left,root2.right) &&
+               isMirror(root1.right, root2.left);
+    }
+    // return true/false denoting whether the tree is Symmetric or not
+    public static boolean isSymmetric(Node root)
+    {
+        // add your code here;
+        if(root == null)
+           return true;
+        return isMirror(root.left,root.right);
+    }
+}
