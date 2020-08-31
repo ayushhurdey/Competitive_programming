cf7a7836220e23fa63d84adddc7a44b909cdb491
Ayush Choudhary <ayushhurdey999@gmail.com>
2020-08-31 23:59:59 +0530

    Added Question

--- tree/binary Tree/lowestCommonAncestor.java
+++ tree/binary Tree/lowestCommonAncestor.java
@@ -1,4 +1,40 @@
+/*
+Lowest Common Ancestor in a Binary Tree 
+Given a Binary Tree with all unique values and two nodes value n1 and n2. The task is to find the lowest common ancestor of the given two nodes. We may assume that either both n1 and n2 are present in the tree or none of them is present. 
 
+Example 1:
+
+Input:
+n1 = 2 , n2 =  3
+
+     1
+   /  \
+  2    3
+Output: 1
+
+Example 2:
+
+Input:
+n1 = 3 , n2 = 4
+
+         5
+        /
+       2
+     /  \
+    3    4
+Output: 2
+Your Task:
+ Just complete the function lca() that takes nodes, n1, and n2 as parameters and returns LCA node as output.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(H).
+Note: H is the height of the tree.
+
+Constraints:
+1 <= Number of nodes <= 100
+1 <= Data of a node <= 1000
+ 
+*/
 
 class Tree
 {
