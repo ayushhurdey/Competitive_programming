dc09249962f3f7957ebe0f59f39be875b2b97104
Ayush Choudhary <ayushhurdey999@gmail.com>
2021-01-25 22:37:22 +0530

    Update KadanesAlgorithm.cpp

--- Dynamic Programming/KadanesAlgorithm.cpp
+++ Dynamic Programming/KadanesAlgorithm.cpp
@@ -5,10 +5,14 @@ Problems
 Given an array arr of N integers. Find the contiguous sub-array with maximum sum.
 
 Input:
-The first line of input contains an integer T denoting the number of test cases. The description of T test cases follows. The first line of each test case contains a single integer N denoting the size of array. The second line contains N space-separated integers A1, A2, ..., AN denoting the elements of the array.
+The first line of input contains an integer T denoting the number of test cases. 
+The description of T test cases follows. The first line of each test case contains
+a single integer N denoting the size of array. The second line contains N 
+space-separated integers A1, A2, ..., AN denoting the elements of the array.
 
 Output:
-Print the maximum sum of the contiguous sub-array in a separate line for each test case.
+Print the maximum sum of the contiguous sub-array in a separate line for each 
+test case.
 
 Constraints:
 1 ≤ T ≤ 110
@@ -27,7 +31,8 @@ Output
 -1
 
 Explanation:
-Testcase 1: Max subarray sum is 9 of elements (1, 2, 3, -2, 5) which is a contiguous subarray.
+Testcase 1: Max subarray sum is 9 of elements (1, 2, 3, -2, 5) which is a 
+contiguous subarray.
 */
 
 
