a15a00c30dbcd52eb0d4df24c9b79886a419844e
Ayush Choudhary <ayushhurdey999@gmail.com>
2021-03-19 00:26:38 +0530

    Update iterativePreOrder.java

--- tree/binary Tree/iterativePreOrder.java
+++ tree/binary Tree/iterativePreOrder.java
@@ -25,3 +25,37 @@ class BinaryTree
         return list;
     }
 }
+
+
+// OR
+
+
+
+class Tree
+{
+    // Return a list containing the inorder traversal of the given tree
+    ArrayList<Integer> preOrder(Node root)
+    {
+        // Code
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        if(root == null)
+            return list;
+        
+        Stack<Node> stack = new Stack<Node>();
+        stack.push(root);
+        
+        while(!stack.isEmpty()){
+            Node node = stack.peek();
+            list.add(node.data);
+            stack.pop();
+            
+            if(node.right != null)
+                stack.push(node.right);
+            if(node.left != null)
+                stack.push(node.left);
+        }
+        return list;
+    }
+    
+    
+}
