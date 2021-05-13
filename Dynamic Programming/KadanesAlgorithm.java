fd81904238554b07c7583839494c115aed02cd0c
Ayush Choudhary <ayushhurdey999@gmail.com>
2021-05-13 22:56:39 +0530

    Update KadanesAlgorithm.java

--- Dynamic Programming/KadanesAlgorithm.java
+++ Dynamic Programming/KadanesAlgorithm.java
@@ -48,10 +48,24 @@ class Solution {
         
         for(int i = 1; i < prices.length; i++){
             currentMax = Math.max(0, currentMax += prices[i] - prices[i-1]);
-            maxSoFar = Math.max(currentMax, maxSoFar);
-            
+            maxSoFar = Math.max(currentMax, maxSoFar);    
         }
-        
         return maxSoFar;
+    }
+}
+
+
+// Leetcode Solution
+public class Solution {
+    public int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
     }
 }
