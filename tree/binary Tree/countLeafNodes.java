4009994514289734223038d4d9ddf466c6806dda
Ayush Choudhary <ayushhurdey999@gmail.com>
2021-06-17 21:48:09 +0530

    Update countLeafNodes.java

--- tree/binary Tree/countLeafNodes.java
+++ tree/binary Tree/countLeafNodes.java
@@ -68,3 +68,20 @@ class Tree
          return count;
     }
 }
+
+
+
+/*-----------------------Simple and Crisp solution----------------*/
+
+class Tree
+{   
+    int countLeaves(Node node) 
+    {
+         // Your code
+         if(node == null)
+            return 0;
+         if(node.left == null && node.right == null)
+            return 1;
+         return countLeaves(node.left) + countLeaves(node.right);
+    }
+}
