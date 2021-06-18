f02aa5c333f8c3f30fd3074be0d5c7a9335bbf97
Ayush Choudhary <ayushhurdey999@gmail.com>
2021-06-18 17:42:56 +0530

    Add iterative inorder solution

--- tree/binary Tree/isBinaryTreeBST.java
+++ tree/binary Tree/isBinaryTreeBST.java
@@ -160,3 +160,25 @@ public class Solution {
    
    if any node violates these two properties is violates BST properties also.
 */
+
+
+
+
+////////////////// Iterative inorder Solution ///////////////
+
+public boolean isValidBST(TreeNode root) {
+   if (root == null) return true;
+   Stack<TreeNode> stack = new Stack<>();
+   TreeNode pre = null;
+   while (root != null || !stack.isEmpty()) {
+      while (root != null) {
+         stack.push(root);
+         root = root.left;
+      }
+      root = stack.pop();
+      if(pre != null && root.val <= pre.val) return false;
+      pre = root;
+      root = root.right;
+   }
+   return true;
+}
