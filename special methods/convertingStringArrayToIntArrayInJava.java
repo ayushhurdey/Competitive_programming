b1b534060362e4b33492f02e9f0c49f715a77cfc
Ayush Choudhary <ayushhurdey999@gmail.com>
2021-09-04 11:48:56 +0530

    Update convertingStringArrayToIntArrayInJava.java

--- special methods/convertingStringArrayToIntArrayInJava.java
+++ special methods/convertingStringArrayToIntArrayInJava.java
@@ -4,5 +4,6 @@ Scanner scan = new Scanner(System.in);
 int[] arr = Arrays.stream(scan.nextLine()
                                       .trim()
                                       .split(" "))
+                                      .filter(x -> !x.equals(""))
                                       .mapToInt(Integer::parseInt)
                                       .toArray();
