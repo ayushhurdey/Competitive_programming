fae7f31060d55a1fd4709a40a73a01db3579be8c
Ayush Choudhary <ayushhurdey999@gmail.com>
2022-02-20 23:09:07 +0530

    Update mergeTwoSortedLL.java

--- Linked Lists/mergeTwoSortedLL.java
+++ Linked Lists/mergeTwoSortedLL.java
@@ -87,3 +87,48 @@ Explanation:
 TIP: Is is always beneficial to take into consideration the use of dummyNode in LL 
      to make things simple, short and fast.
 */
+
+
+
+
+// Solution:
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode ptr = dummy;
+        
+        while(list1 != null && list2 != null){
+            if(list1.val > list2.val){
+                ptr.next = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+            else{
+                ptr.next = new ListNode(list1.val);
+                list1 = list1.next;
+            }
+             ptr = ptr.next;
+        }
+        
+        if(list1 != null){
+           ptr.next = list1;
+        }
+        else{
+            ptr.next = list2;
+        }
+        
+        return dummy.next;
+    }
+}
+
