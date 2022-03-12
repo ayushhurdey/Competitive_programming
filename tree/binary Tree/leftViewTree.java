04738df0b15a0ae76e94bacff38a6d46a5952b47
Ayush Choudhary <ayushhurdey999@gmail.com>
2022-03-12 19:00:49 +0530

    Add DFS method for left view of binary tree

--- tree/binary Tree/leftViewTree.java
+++ tree/binary Tree/leftViewTree.java
@@ -75,3 +75,63 @@ Hint: Draw Horizontal levels. Starting with -1(i.e null level).
         that will be helpful when right subtree's height > left subtree height.
         Increase level each time you traverse to left or right subtree.
 */
+
+
+
+
+// DFS way of Left View of Binary Tree - Simpler to understand. - Similar to Right View
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    List<Integer> leftViewList = new ArrayList<>();
+    Map<Integer, Integer> map = new HashMap<>();
+    int maxLevel = 0;
+    
+    public void getLeftSideView(TreeNode root, int level){
+        if(root == null)
+            return;
+        
+        // updating map at each node moving right to left at a particlar level.
+        map.put(level, root.val);  
+        maxLevel = Math.max(maxLevel, level);  //  keeping maxLevel to use in list to add elements from map
+        
+        getLeftSideView(root.right, level+1);   // going right first
+        getLeftSideView(root.left, level+1);  // then going to left so that leftmost is updated at the last.
+    }
+    
+    public List<Integer> leftSideView(TreeNode root) {
+        // Write your code here
+        if(root == null)
+            return new ArrayList<>();
+
+        getLeftSideView(root, 0);
+        for(int i = 0; i <= maxLevel; i++){
+            leftViewList.add(map.get(i));    
+        }
+
+        return leftViewList;
+    }
+}
+
+
+/* Approach:
+    While visiting each level again and again from right to left
+    Keep updating the value corresponding to each level.
+    Finally the map will give the leftmost nodes at each level
+    in the binary tree.
+*/
