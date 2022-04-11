f3cd34142453386b48d5fc4b37b4738649ccb74b
Ayush Choudhary <ayushhurdey999@gmail.com>
2022-04-11 09:59:28 +0530

    Add Iterative solution for IsSymmetricTree

--- tree/binary Tree/isSymmetricTree.java
+++ tree/binary Tree/isSymmetricTree.java
@@ -53,3 +53,35 @@ class GfG
         return isMirror(root.left,root.right);
     }
 }
+
+
+
+// Iterative solution using Queue
+
+
+
+public boolean isSymmetric(TreeNode root) {
+        if(root == null)
+            return true;
+        Queue<TreeNode> q = new LinkedList();
+       
+        q.add(root.left);
+        q.add(root.right);
+        while(!q.isEmpty()){
+            TreeNode left = q.poll();
+            TreeNode right = q.poll();
+            if(left == null && right == null)
+                continue;
+            if(left == null || right == null ||left.val != right.val )
+                return false;
+            q.add(left.left);
+            q.add(right.right);
+            q.add(left.right);
+            q.add(right.left);
+            
+        }
+        return true;
+            
+    }
+
+
