d9dc5c6d189dfba2a392d15e6ca3ccb6e6f1e8e7
Ayush Choudhary <ayushhurdey999@gmail.com>
2022-05-14 21:15:31 +0530

    Add another solution to IntersectionPointOfLinkedLists

--- Linked Lists/intersectionPointOfLinkedLists.java
+++ Linked Lists/intersectionPointOfLinkedLists.java
@@ -101,3 +101,37 @@ class Intersect
 }
 
 
+
+// Another Simple solution to find intersection point with two iterations only.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null)
+            return null;
+        ListNode tempHeadA = headA;
+        ListNode tempHeadB = headB;
+        
+        //if a & b have different len, then we will stop the loop after second iteration
+        while(tempHeadA != tempHeadB){
+            //for the end of first iteration, we just reset the pointer to the head of another linkedlist
+            tempHeadA = tempHeadA == null ? headB : tempHeadA.next;
+            tempHeadB = tempHeadB == null ? headA : tempHeadB.next;
+        }
+        
+        return tempHeadA;
+    }
+}
+
